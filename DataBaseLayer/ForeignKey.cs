981104f2d5c1a719c35331d9e4190950d3dc6658
Alejandro Ferrera <aleferrera@hotmail.com>
2017-05-12 12:35:14 -0600

    Generate foreign key DLL

--- DataBaseLayer/ForeignKey.cs
+++ DataBaseLayer/ForeignKey.cs
@@ -3,19 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IBM.Data.DB2;
 
 namespace DataBaseLayer
 {
     public class ForeignKey : Constraints
     {
+        private readonly string _schema;
+        private readonly string _name;
+        private readonly string _table;
+
         public ForeignKey(string schema, string name, string table)
         {
-            throw new NotImplementedException();
+            _schema = schema;
+            _name = name;
+            _table = table;
         }
 
         public override string GenerateDDL()
         {
-            throw new NotImplementedException();
+            var queryRefTable =
+                $"SELECT REFTABNAME FROM SYSCAT.REFERENCES WHERE TABSCHEMA = '{_schema}' AND CONSTNAME = '{_name}'";
+
+            var queryRefKey =
+                $"SELECT REFKEYNAME FROM SYSCAT.REFERENCES WHERE TABSCHEMA = '{_schema}' AND CONSTNAME = '{_name}'";
+
+            var queryColName = $"SELECT COLNAME FROM SYSCAT.KEYCOLUSE WHERE CONSTNAME = '{_name}'";
+
+            var refTableReader = new DB2Command(queryRefTable, Connection.CurrentConnection).ExecuteReader();
+            var refKeyReader = new DB2Command(queryRefKey, Connection.CurrentConnection).ExecuteReader();
+            var colNameReader = new DB2Command(queryColName, Connection.CurrentConnection).ExecuteReader();
+
+            refTableReader.Read();
+            refKeyReader.Read();
+
+            var refColNameQuery = $"SELECT COLNAME FROM SYSCAT.KEYCOLUSE WHERE CONSTNAME = '{refKeyReader.GetString(0)}'";
+            var refColReader = new DB2Command(refColNameQuery, Connection.CurrentConnection).ExecuteReader();
+
+            var fkDll = $"ALTER TABLE {_table}\nADD CONSTRAINT {_name} FOREIGN KEY(";
+
+            while (colNameReader.Read())
+            {
+                fkDll += colNameReader["COLNAME"] + ", ";
+            }
+
+            fkDll = fkDll.Substring(0, fkDll.Length - 2);
+            fkDll += $") REFERENCES {refTableReader.GetString(0)} (";
+
+            while (refColReader.Read())
+            {
+                fkDll += refColReader["COLNAME"] + ", ";
+            }
+
+            fkDll = fkDll.Substring(0, fkDll.Length - 2);
+            fkDll += ");";
+
+            refTableReader.Close();
+            refKeyReader.Close();
+            refColReader.Close();
+            colNameReader.Close();
+
+            return fkDll;
         }
 
         public override string GenerateDropDDL()
